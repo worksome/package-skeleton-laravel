dd1986d408956687d944ed0dc4e22f8069eb27ea
Owen Voke <development@voke.dev>
2024-02-08 11:08:30 +0000

    feat: add support for Laravel 11

--- src/Facades/Skeleton.php
+++ src/Facades/Skeleton.php
@@ -13,6 +13,6 @@ class Skeleton extends Facade
 {
     protected static function getFacadeAccessor()
     {
-        return 'skeleton';
+        return \VendorName\Skeleton\Skeleton::class;
     }
 }
